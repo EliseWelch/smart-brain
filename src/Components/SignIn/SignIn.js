746f526706dcc39df6fb9dfe2377fd9d76039abf
Elise Welch <eliseanyawelch@gmail.com>
2021-02-08 16:04:31 -0500

    changed fetch urls

--- src/Components/SignIn/SignIn.js
+++ src/Components/SignIn/SignIn.js
@@ -18,7 +18,7 @@ class SignIn extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://mighty-springs-38949.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
